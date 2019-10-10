8819699d699065d3304b2d60c63859e0e1a42f14
bhaveshrajrathore <54987558+bhaveshrajrathore@users.noreply.github.com>
2019-10-10 10:48:06 +0530

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,1 +1,9 @@
-
+pipelineJob ('ZingUpLife-DSL') {
+     def gitUrl = 'https://github.com/bhaveshrajrathore/ZApp.git'
+     definition {
+                    cps {
+                         script(readFileFromWorkspace('Jenkinsfile'))
+                         sandbox()     
+                    }
+     }
+}
